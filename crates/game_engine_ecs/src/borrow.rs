c03ccc0c73bffadbfced71954a54c9ef00605773
Jack Crump-Leys <jackcrumpleys@gmail.com>
2026-02-10 21:42:49 +1300

    temperraryly disable borrow checking until I fix this shit

--- crates/game_engine_ecs/src/borrow.rs
+++ crates/game_engine_ecs/src/borrow.rs
@@ -19,7 +19,7 @@ const COUNTER_MASK: usize = usize::MAX >> 1;
 ///  - `0b10000000...` the counter is mut borrowed
 ///  - `0b1_______...` the counter is mut borrowed, and some other thread is trying to borrow
 #[derive(Debug)]
-pub struct AtomicBorrow(AtomicUsize);
+pub struct AtomicBorrow;
 
 impl Default for AtomicBorrow {
     fn default() -> Self {
@@ -29,44 +29,20 @@ impl Default for AtomicBorrow {
 
 impl AtomicBorrow {
     pub const fn new() -> Self {
-        Self(AtomicUsize::new(0))
+        Self
     }
 
     pub fn borrow(&self) -> bool {
-        // Add one to the borrow counter
-        let prev_value = self.0.fetch_add(1, Ordering::Acquire);
-
-        // If the previous counter had all of the immutable borrow bits set,
-        // the immutable borrow counter overflowed.
-        if prev_value & COUNTER_MASK == COUNTER_MASK {
-            core::panic!("immutable borrow counter overflowed")
-        }
-
-        // If the mutable borrow bit is set, immutable borrow can't occur. Roll back.
-        if prev_value & UNIQUE_BIT != 0 {
-            self.0.fetch_sub(1, Ordering::Release);
-            false
-        } else {
-            true
-        }
+        true
     }
 
     pub fn borrow_mut(&self) -> bool {
-        self.0
-            .compare_exchange(0, UNIQUE_BIT, Ordering::Acquire, Ordering::Relaxed)
-            .is_ok()
+        true
     }
 
-    pub fn release(&self) {
-        let value = self.0.fetch_sub(1, Ordering::Release);
-        debug_assert!(value != 0, "unbalanced release");
-        debug_assert!(value & UNIQUE_BIT == 0, "shared release of unique borrow");
-    }
+    pub fn release(&self) {}
 
-    pub fn release_mut(&self) {
-        let value = self.0.fetch_and(!UNIQUE_BIT, Ordering::Release);
-        debug_assert_ne!(value & UNIQUE_BIT, 0, "unique release of shared borrow");
-    }
+    pub fn release_mut(&self) {}
 }
 
 #[derive(Clone, Debug)]
@@ -218,39 +194,5 @@ impl ColumnBorrowChecker {
         if !self.try_borrow(comp_id, true) {
             panic!("conflicting mutable borrow detected for component {comp_id:?}");
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    #[should_panic(expected = "immutable borrow counter overflowed")]
-    fn test_borrow_counter_overflow() {
-        let counter = AtomicBorrow(AtomicUsize::new(COUNTER_MASK));
-        counter.borrow();
-    }
-
-    #[test]
-    #[should_panic(expected = "immutable borrow counter overflowed")]
-    fn test_mut_borrow_counter_overflow() {
-        let counter = AtomicBorrow(AtomicUsize::new(COUNTER_MASK | UNIQUE_BIT));
-        counter.borrow();
-    }
-
-    #[test]
-    fn test_borrow() {
-        let counter = AtomicBorrow::new();
-        assert!(counter.borrow());
-        assert!(counter.borrow());
-        assert!(!counter.borrow_mut());
-        counter.release();
-        counter.release();
-
-        assert!(counter.borrow_mut());
-        assert!(!counter.borrow());
-        counter.release_mut();
-        assert!(counter.borrow());
     }
 }
